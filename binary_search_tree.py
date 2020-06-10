af08374874b2ead87ab2bde6a198e5244e9cca8d
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-06-10 22:47:15 +0400

    Add files via upload

---
+++ binary_search_tree.py
@@ -0,0 +1,32 @@
+# Binary Search Trees
+
+
+class Node:
+    def __init__(self, data):
+        self.right = self.left = None
+        self.data = data
+
+
+class Solution:
+    def insert(self, root, data):
+        if root is None:
+            return Node(data)
+        else:
+            if data <= root.data:
+                current = self.insert(root.left, data)
+                root.left = current
+            else:
+                current = self.insert(root.right, data)
+                root.right = current
+        return root
+
+    def level_of_order(self, root):
+        pass
+
+
+n = int(input())
+biTree = Solution()
+root = None
+for i in range(n):
+    data = int(input())
+    root = biTree.insert(root, data)
