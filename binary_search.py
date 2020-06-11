309b97e29e4b067205407fed7dca2872a2fd8eb2
Said Remiev <59606092+SaidRem@users.noreply.github.com>
2020-06-11 21:51:35 +0400

    Add files via upload

---
+++ binary_search.py
@@ -0,0 +1,21 @@
+def binary_search(arr, target):
+    high = len(arr) - 1
+    low = 0
+    while low <= high:
+        mid = (high + low) // 2
+        res = arr[mid]
+        if target > res:
+            low = mid + 1
+            continue
+        elif target < res:
+            high = mid - 1
+            continue
+        return 'Target found'
+    return 'target not found'
+
+
+if __name__ == '__main__':
+    arr = list(map(int, input('enter integers in order (separated by spaces): '
+                              ).strip().split()))
+    target = int(input('enter number: '))
+    print(binary_search(arr, target))
